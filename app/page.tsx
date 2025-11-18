c1723a559eefe4d4af07249273daeba53a1ee226
adrian castillo <asidebk@Mac.lan>
2025-11-18 16:47:49 -0500

    Update site

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,7 @@ export default function HomePage() {
   return (
     <div className="relative w-full h-screen overflow-hidden">
       {/* Logo in the top-left */}
-      <img
-        src="/abc_logo.png" // Replace with your actual logo path
-        alt="Logo"
-        className="absolute top-4 left-4 w-64 h-auto z-10"
-      />
+     
 
       {/* 3D Scene */}
       <Scene />
